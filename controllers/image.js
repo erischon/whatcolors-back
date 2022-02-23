c851b6184f9a0b46824395abd6a5bb225ecdb832
Eri Schon <erischon@gmail.com>
2022-02-23 12:27:12 +0100

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -11,7 +11,7 @@ const handleApiCall = (req, res) => {
     stub.PostModelOutputs(
         {
             // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-            model_id: "a403429f2ddf4b49b307e318f00e528b",
+            model_id: "eeed0b6733a644cea07cf4c60f87ebb7",
             inputs: [{data: {image: { url: req.body.input }}}]
         },
         metadata,
@@ -35,20 +35,6 @@ const handleApiCall = (req, res) => {
     );
 
 }
-
-// const Clarifai = require('clarifai')
-// console.log(Clarifai)
-
-// const app = new Clarifai.App({
-//     apiKey: "55b42ee16bf44ffa8bfc50e53ffd04fd"
-// })
-
-// const handleApiCall = (req, res) => {
-//     app.models
-//         .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-//         .then(data => { res.json(data) })
-//         .catch(err => { res.status(400).json("Unable to work with API") })   
-// }
 
 const handleImage = (req, res, db) => {
     const { id } = req.body
