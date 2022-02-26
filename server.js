5ad6ad44013649c939a57265058ece1f2625b65a
Eri Schon <erischon@gmail.com>
2022-02-26 09:41:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const signin = require("./controllers/signin")
 const profile = require("./controllers/profile")
 const image = require("./controllers/image")
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const db = knex({
     client: "pg",
     connection: {
-        // connectionString: process.env.DATABASE_URL,
-        // ssl: true
-        host: "localhost",
-        port: 5433,
-        user: "admin",
-        password: "secret",
-        database: "smart-brain"
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+        // host: "localhost",
+        // port: 5433,
+        // user: "admin",
+        // password: "secret",
+        // database: "smart-brain"
     }
 })
 
